e6b1ac7b7e2b20040bb71c58eaedc6b5e8798af5
jeetendrab <42413277+jeetendrab@users.noreply.github.com>
2018-11-05 14:50:07 +0530

    Add files via upload

---
+++ multi-folder.ps1
@@ -0,0 +1,8 @@
+﻿$basefolder = 'C:\Temp\userhome'
+$employees = Import-Csv -Path C:\Temp\employee.csv
+foreach($employee in $employees){
+$username="$($employee.firstname.Substring(0,1))$($employee.lastname)"
+
+New-Item -Path $basefolder -Name $username -Type directory
+}
+
