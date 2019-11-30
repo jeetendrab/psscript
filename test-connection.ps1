be8905d18ce38743e9cd6f377e3cd11225ed9316
Jeetendra <42413277+jeetendrab@users.noreply.github.com>
2019-11-30 13:42:52 +0530

    test the connectivity of Multiple computers

---
+++ test-connection.ps1
@@ -0,0 +1,14 @@
+﻿$Computers = Get-Content C:\temp\Ping.txt
+Foreach ($Computer in $Computers)
+{
+
+    if (Test-Connection -ComputerName $Computer -Count 1 -Quiet)
+    {
+    "Connection is okay $Computer"
+     
+    } else {
+ 
+  Write-Host  "OH! No ICMP to $Computer" -ForegroundColor Red 
+       
+    }
+    }
